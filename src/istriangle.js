8cd322cd6f4f281892a2dce23828340633176ba1
Dhruvakgowda <dhruva0805@gmail.com>
2021-10-08 07:27:56 +0530

    negatuve

--- src/istriangle.js
+++ src/istriangle.js
@@ -7,13 +7,18 @@ var inputs = document.querySelectorAll(".angle-input");
 function isTriangleHandler() {
   var total = 0;
   for (var i = 0; i < inputs.length; i++) {
-    total = total + Number(inputs[i].value);
-  }
-  if (total === 180) {
-    message.innerText = "Triangle can be formed with these angles";
-  } else {
-    message.innerText = "Triangle cannot be formed with these angles";
+    if (inputs[i].value <= 0) {
+      //console.log("hey");
+      message.innerText = "Enter positive angles";
+    } else {
+      total = total + Number(inputs[i].value);
+      //console.log(inputs[i].value);
+      if (total === 180) {
+        message.innerText = "Triangle can be formed with these angles";
+      } else {
+        message.innerText = "Triangle cannot be formed with these angles";
+      }
+    }
   }
 }
-
 button.addEventListener("click", isTriangleHandler);
